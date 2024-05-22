2c9213cb9f250b2154f760c820804f7d41703646
Deftly <deftkz@gmail.com>
2024-05-21 21:19:22 -0700

    initial routing structure + packages

--- reportingClient/src/App.jsx
+++ reportingClient/src/App.jsx
@@ -1,3 +1,36 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<div>App Layout</div>}>
+            <Route index element={<div>Dashboard</div>} />
+            <Route path="workflows" element={<div>Workflows</div>} />
+            <Route
+              path="workflows/:hostname/:txId/stages"
+              element={<div>Workflow stages</div>}
+            />
+            <Route path="hosts" element={<div>Hosts</div>} />
+          </Route>
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+      {/*TODO: Add <Toaster /> if using react-hot-toast for notifications*/}
+    </QueryClientProvider>
+  );
+};
+
+export default App;
