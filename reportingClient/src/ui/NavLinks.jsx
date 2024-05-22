749d8a168d45fad51705e5676b987e1f68cbc441
Deftly <deftkz@gmail.com>
2024-05-22 10:49:26 -0700

    disable click for selected buttons - sidebar

--- reportingClient/src/ui/NavLinks.jsx
+++ reportingClient/src/ui/NavLinks.jsx
@@ -1,4 +1,5 @@
 import { NavLink, useLocation } from "react-router-dom";
+import { useState } from "react";
 import clsx from "clsx";
 import {
   AdjustmentsHorizontalIcon,
@@ -16,10 +17,18 @@ const links = [
 
 const NavLinks = () => {
   const location = useLocation();
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+        const isActive = activeButton === link.name;
+
         return (
           <NavLink
             key={link.name}
@@ -27,11 +36,13 @@ const NavLinks = () => {
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               "transition duration-300 ease-in-out transform hover:-translate-y-1",
-              "shadow-md hover:shadow-lg", // Added box shadow classes
+              "shadow-md hover:shadow-lg",
               {
                 "bg-sky-100 text-blue-600": location.pathname === link.href,
+                "pointer-events-none": isActive,
               },
             )}
+            onClick={() => handleButtonClick(link.name)}
           >
             <LinkIcon className="w-6" />
             <p className="hidden pl-3 md:block">{link.name}</p>
