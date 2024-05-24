6c5d7db3ffdc80be828783bf2fccba187ed2fa6b
Deftly <deftkz@gmail.com>
2024-05-24 10:32:09 -0700

    pagination component added

--- reportingClient/src/pages/Workflows.jsx
+++ reportingClient/src/pages/Workflows.jsx
@@ -1,23 +1,58 @@
-import { useMemo } from "react";
-import { useElasticsearchWorkflows } from "../hooks/useWorkflows";
+import { useMemo, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
+import { DOCS_PER_PAGE } from "../utils/constants";
+import { useElasticsearchWorkflows } from "../hooks/useWorkflows";
 import WorkflowTableV2 from "../features/workflows/WorkflowTableV2";
 
 import Heading from "../ui/Heading";
+import Pagination from "../ui/Pagination";
 
 const Workflows = () => {
-  const { isLoading, data } = useElasticsearchWorkflows();
+  const navigate = useNavigate();
+  const queryParams = useQueryParams();
+
+  const [currentPage, setCurrentPage] = useState(
+    parseInt(queryParams.page) || 1,
+  );
+
+  const { isLoading, data } = useElasticsearchWorkflows(currentPage - 1);
 
   const workflows = useMemo(() => data?.workflows || [], [data]);
+  const totalHits = data?.totalHits || 0;
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    const newSearchParams = new URLSearchParams(queryParams);
+    newSearchParams.set("page", newPage);
+    navigate(`${location.pathname}?${newSearchParams.toString()}`);
+  };
 
   return (
     <div className="mt-1">
-      <div className="flex justify-between items-end mb-8">
+      <div className="flex justify-between items-end mb-4">
         <Heading as="h1">Workflows</Heading>
       </div>
       <WorkflowTableV2 workflows={workflows} isLoading={isLoading} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(totalHits / DOCS_PER_PAGE)}
+        onPageChange={handlePageChange}
+        totalItems={totalHits}
+        itemsPerPage={DOCS_PER_PAGE}
+      />
     </div>
   );
+};
+
+const useQueryParams = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const queryParams = {};
+  for (const [key, value] of searchParams.entries()) {
+    queryParams[key] = value;
+  }
+  return queryParams;
 };
 
 export default Workflows;
---
+++ reportingClient/src/ui/Pagination.jsx
@@ -0,0 +1,93 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  totalItems,
+  itemsPerPage,
+}) => {
+  const startIndex = (currentPage - 1) * itemsPerPage + 1;
+  const endIndex = Math.min(currentPage * itemsPerPage, totalItems);
+
+  return (
+    <div className="flex items-center justify-between bg-white mt-8 px-4 py-3 sm:px-6">
+      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          {endIndex === 0 ? (
+            <p className="text-sm text-gray-700"></p>
+          ) : (
+            <p className="text-sm text-gray-700">
+              Showing <span className="font-medium">{startIndex}</span> to{" "}
+              <span className="font-medium">{endIndex}</span> of{" "}
+              <span className="font-medium">{totalItems}</span> results
+            </p>
+          )}
+        </div>
+        <div>
+          <nav
+            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            aria-label="Pagination"
+          >
+            <button
+              onClick={() => onPageChange(Math.max(currentPage - 1, 1))}
+              disabled={currentPage === 1}
+              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            {/* Render page numbers */}
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => {
+              if (
+                page === 1 ||
+                page === totalPages ||
+                (page >= currentPage - 2 && page <= currentPage + 2)
+              ) {
+                return (
+                  <button
+                    key={page}
+                    onClick={() => onPageChange(page)}
+                    aria-current={page === currentPage ? "page" : undefined}
+                    className={`${
+                      page === currentPage
+                        ? "relative z-10 inline-flex items-center bg-blue-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    }`}
+                  >
+                    {page}
+                  </button>
+                );
+              } else if (
+                (page === currentPage - 3 && page > 1) ||
+                (page === currentPage + 3 && page < totalPages)
+              ) {
+                return (
+                  <span
+                    key={page}
+                    className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700"
+                  >
+                    ...
+                  </span>
+                );
+              }
+              return null;
+            })}
+            <button
+              onClick={() =>
+                onPageChange(Math.min(currentPage + 1, totalPages))
+              }
+              disabled={currentPage === totalPages}
+              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Next</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
